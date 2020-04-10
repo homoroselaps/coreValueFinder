ddda90213653743d48e8d68e8becee39d701fad2
Jan-Tobias Matysik <jan-tobias.matysik@nexenio.com>
2020-04-10 20:54:15 +0200

    fix exclusive value choice

--- main.py
+++ main.py
@@ -49,15 +49,16 @@ def clear(text=""):
 
 
 
-def get_input() -> int:
+def get_input(min_value, max_value) -> int:
     """ Returns a number, if one got 'inputted'.
     Might throw a KeyboardInterrupt.
 
     Args:
-        None
+        min_value: lower bound
+        max_value: upper bound
 
     Returns:
-        int: number between 0 and 3
+        int: number between min_value and max_value inclusive
 
     Throws:
         KeyboardInterrupt, if the user intends to end inputting values
@@ -73,18 +74,15 @@ def get_input() -> int:
 
     try:
         index = int(inp)
-        assert index > -1
-        assert index < 4
+        assert index >= min_value
+        assert index <= max_value
     except (ValueError, AssertionError):
         print(
             "Please enter '1', '2' or '3' to select one of the presented values, or '0' if they are of equal value to you."
         )
-        return get_input()  # this might lead to a very unlikely
+        return get_input(min_value, max_value)  # this might lead to a very unlikely
         # 'recursion limit error' when repeated more than 5k times.
-        # (or whatever is the limit in your current python runtime)
     return index
-
-
 
 def main(showDescr = True):
     print("welcome to the core value finder")
@@ -102,7 +100,7 @@ def main(showDescr = True):
         sessionPath = None
 
     if not sessionPath:
-        sessionPath = ".{}.session".format(time.strftime("%Y-%m-%d-%H:%M"))
+        sessionPath = ".{}.session".format(time.strftime("%Y-%m-%d-%H-%M"))
         print("New Session: {}".format(sessionPath))
         for v in valueset.keys():
             values[v] = 0
@@ -110,7 +108,14 @@ def main(showDescr = True):
     # 2^len(values) comparisons are a good estimate to get solid value differences.
     # can be aborted prior to that, and repeated.
     for _ in range(2 ** len(values)):
-        selection = [choice([*values]), choice([*values]), choice([*values])]
+        #select shown values
+        selection = []
+        for _ in range(0,3):
+            while len(values) > len(selection):
+                newValue = choice([*values])
+                if newValue not in selection:
+                    selection += [newValue]
+                    break
         clear()
         if showDescr:
             for i in range(0, len(selection)):
@@ -120,7 +125,7 @@ def main(showDescr = True):
             print("[1] {0} [2] {1} [3] {2}".format(selection[0], selection[1], selection[2]))
 
         try:
-            index = get_input()
+            index = get_input(1, len(selection))
             if index:  # if index == 0: don't evaluate
                 values[selection[index - 1]] += 1
         except KeyboardInterrupt:
